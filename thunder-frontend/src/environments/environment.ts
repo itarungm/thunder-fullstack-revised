238c834840665749c56750f496242c95577d7e38
Tarun <tarun7mandal@gmail.com>
2022-03-04 13:34:32 +0530

    gitignore updated

--- thunder-frontend/src/environments/environment.ts
+++ thunder-frontend/src/environments/environment.ts
@@ -10,7 +10,6 @@ export const environment = {
   LINK_TREE_VIEWER_URL: `${window.location.protocol}//${window.location.host}/link-tree`,
   SECRET:'thunder',
   IMAGE_HOST_URL: `https://storage.googleapis.com/thunder-official.appspot.com/`,
-  JSON_CRYPTO_SECRET: "d71ec08e16cf9ee8772a9356914e0a7c2543ca82dc0e670cc6051f8dd7d4bc770c4e3d6ac0aa5d998823f5f05174c95a"
 };
 
 /*
